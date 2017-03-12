eeaee0b6215017e36eaa8359712181bc6be73170
devashishkatoriya <devashishkatoriya@gmail.com>
2017-03-12 13:51:41 +0530

    Code Cleanup

--- main.cpp
+++ main.cpp
@@ -15,47 +15,38 @@
  
 using namespace std; 
  
-#define MAX 10 
+#define MAX 10
+#define LIMIT 30
  
 //Class Definitions 
-class Entry                                                          //Entry Class
+class Entry                                                      //Entry Class - Linked List Implementation
 { 
 public : 
-    char word[MAX],meaning[MAX];
+    char word[LIMIT],meaning[LIMIT];
+    Entry *next;
 }; 
  
 class HashTable                                                     //Hash Table Class 
 { 
-    Entry ht[MAX]; 
-    void insert(Entry); 
-    void insertQ(Entry); 
+    Entry *ht[MAX];
+    void insert(Entry *);
     inline int HashFunc(char []); 
 public : 
     HashTable() 
     { 
         for(int i=0;i<MAX;i++) 
-        { 
-            strcpy(ht[i].word,"");
-            strcpy(ht[i].meaning,"");
+        {
+            ht[i] = NULL;
         } 
-    } 
-    bool isFull() 
-    { 
-        for(int i=0;i<MAX;i++) 
-        { 
-            if(strcmp(ht[i].word,"")==0)
-            { 
-                return false; 
-            } 
-        } 
-        return true; 
-    } 
+    }
     void clr() 
     { 
         for(int i=0;i<MAX;i++) 
-        { 
-            strcpy(ht[i].word,"");
-            strcpy(ht[i].meaning,"");
+        {
+            ht[i] = new Entry;
+            strcpy(ht[i]->word,"");
+            strcpy(ht[i]->meaning,"");
+            ht[i]->next = NULL;
         } 
     } 
     void construct(); 
@@ -65,89 +56,33 @@ public :
 }; 
  
 void HashTable::construct()                                           //Function for Insertion 
-{ 
-    if(isFull()) 
-    { 
-        cout<<"\nSorry, the Dictionary is Full.";
-        return; 
-    } 
-     
-    int ch = 0; 
-    cout<<"\n\n 1 for Linear Hashing."; 
-    cout<<"\n 2 for Quadratic Hashing."; 
-    cout<<"\nEnter which Hashing Technique to use ? "; 
-    cin>>ch; 
-    if(ch==1) 
-    { 
-        Entry B; 
-        cout<<"\nEnter Entry Details :- ";
-        cout<<"\nEnter Word   \t: ";
-        cin>>B.word;
-        cout<<"Enter Meaning\t: ";
-        cin>>B.meaning;
-        insert(B); 
-    } 
-    else if(ch==2) 
-    { 
-        Entry B; 
-        cout<<"\nEnter Entry Details :- ";
-        cout<<"\nEnter Word   \t: ";
-        cin>>B.word;
-        cout<<"Enter Meaning\t: ";
-        cin>>B.meaning;
-        insertQ(B); 
-    } 
-    else 
-        cout<<"\nInvalid Option!"; 
+{
+    Entry *B;
+    B = new Entry();
+    cout<<"\nEnter Entry Details :- ";
+    cout<<"\nEnter Word   \t: ";
+    cin>>B->word;
+    cout<<"Enter Meaning\t: ";
+    cin>>B->meaning;
+    B->next = NULL;
+    insert(B);
 } 
  
-void HashTable::insert(Entry B)                                         //Insertion using Linear Probing 
+void HashTable::insert(Entry *B)                                         //Insertion
 { 
     int pos; 
-    pos = HashFunc(B.word);
-    if(strcmp(ht[pos].word,"")==0)
-    { 
+    pos = HashFunc(B->word);
+    if(ht[pos]==NULL)
+    {
         ht[pos] = B;
-        cout<<"\nWord inserted Successfully!";
-    } 
-    else 
-    { 
-        int i=pos; 
-        do 
-        { 
-            if(strcmp(ht[i].word,"")==0)
-            { 
-                ht[i] = B;
-                cout<<"\nWord inserted Successfully!";
-                break; 
-            } 
-            i=(i+1)%MAX; 
-        }while(i!=pos); 
-    } 
-} 
- 
-void HashTable::insertQ(Entry B)                                         //Insertion using Quadratic Probing 
-{ 
-    int pos; 
-    pos = HashFunc(B.word);
-    if(strcmp(ht[pos].word,"")==0)
-    { 
-        ht[pos] = B;
-        cout<<"\nWord inserted Successfully!";
-    } 
-    else 
-    { 
-        for(int i=1; i<(MAX-1)/2; i++) 
-        { 
-            if(strcmp(ht[(pos+(i*i))%MAX].word,"")==0)
-            { 
-                ht[(pos+(i*i))%MAX] = B;
-                cout<<"\nWord inserted Successfully!";
-                break; 
-            } 
-        } 
-    } 
-} 
+    }
+    else
+    {
+        B->next = ht[pos]->next;
+        ht[pos]->next = B;
+    }
+    cout<<"\nWord inserted Successfully!";
+}
  
 inline int HashTable::HashFunc(char key[MAX])                             //Hash Function
 { 
@@ -158,15 +93,22 @@ inline int HashTable::HashFunc(char key[MAX])                             //Hash
 } 
  
 void HashTable::display()                                               //Display
-{ 
+{
+    Entry *curr;
     cout<<"\n\t\tDictionary";
     cout<<"\n---------------------------------------------"; 
-    cout<<"\nPos.\tWord\t\tMeaning";
+    cout<<"\nWord - Meaning";
     cout<<"\n---------------------------------------------"; 
     for(int i=0;i<MAX;i++) 
-    { 
-        cout<<"\n"<<i<<"\t"<<ht[i].word<<"\t\t"<<ht[i].meaning;
-    } 
+    {
+        curr = ht[i];
+        cout<<"\n";
+        while(curr!=NULL)
+        {
+            cout<<curr->word<<" - "<<curr->meaning<<", ";
+            curr = curr->next;
+        }
+    }
     cout<<"\n---------------------------------------------"; 
     cout<<"\nMAX is "<<MAX; 
 } 
@@ -178,25 +120,26 @@ void HashTable::search()                                                //Search
     cout<<"\nEnter word to Search : ";
     cin>>word;
     pos = HashFunc(word);
-    if(strcmp(ht[pos].word,word)==0)
+    if(strcmp(ht[pos]->word,word)==0)
     {
-        cout<<"\nWord   \t: "<<ht[pos].word;
-        cout<<"\nMeaning\t: "<<ht[pos].meaning;
+        cout<<"\nWord   \t: "<<ht[pos]->word;
+        cout<<"\nMeaning\t: "<<ht[pos]->meaning;
         return; 
     } 
     else 
     { 
-        int i=pos; 
-        do 
-        { 
-            if(strcmp(ht[i].word,word)==0)
+        Entry *temp;
+        temp = ht[pos]->next;
+        while(temp!=NULL)
+        {
+            if (strcmp(temp->word, word) == 0)
             {
-                cout << "\nWord   \t: " << ht[i].word;
-                cout << "\nMeaning\t: " << ht[i].meaning;
-                return; 
-            } 
-            i=(i+1)%MAX; 
-        }while(i!=pos); 
+                cout << "\nWord   \t: " << temp->word;
+                cout << "\nMeaning\t: " << temp->meaning;
+                return;
+            }
+            temp = temp->next;
+        }
         cout<<"\nWord NOT found!";
     } 
 }
@@ -208,29 +151,27 @@ void HashTable::delete_word()                                                //D
     cout<<"\nEnter word to Delete : ";
     cin>>word;
     pos = HashFunc(word);
-    if(strcmp(ht[pos].word,word)==0)
+    if(strcmp(ht[pos]->word,word)==0)
     {
-        strcpy(ht[pos].word,"");
-        strcpy(ht[pos].meaning,"");
+        ht[pos] = ht[pos]->next;
         cout<<"\nWord deleted Successfully!";
         return;
     }
-    else
+    Entry *temp,*prev;
+    prev = ht[pos];
+    temp = prev->next;
+    while(temp!=NULL)
     {
-        int i=pos;
-        do
+        if (strcmp(temp->word, word) == 0)
         {
-            if(strcmp(ht[i].word,word)==0)
-            {
-                strcpy(ht[i].word,"");
-                strcpy(ht[i].meaning,"");
-                cout<<"\nWord deleted Successfully!";
-                return;
-            }
-            i=(i+1)%MAX;
-        }while(i!=pos);
-        cout<<"\nWord NOT found!";
+            prev->next = temp->next;
+            cout<<"\nWord deleted Successfully!";
+            return;
+        }
+        temp = temp->next;
+        prev = prev->next;
     }
+    cout<<"\nWord NOT found!";
 }
  
 //main() 
@@ -249,7 +190,6 @@ int main()
         cout<<"\n 2 to Display Dictionary";
         cout<<"\n 3 to Search a word";
         cout<<"\n 4 to Delete a word";
-        cout<<"\n 5 for IsFull";
         cout<<"\n -1 to Clear whole Dictionary";
         cout<<"\n  0 to Quit"; 
         cout<<"\nEnter your choice : "; 
@@ -268,12 +208,6 @@ int main()
             case 4 :
                 obj.delete_word();
                 break;
-            case 5 :
-                if(obj.isFull()) 
-                    cout<<"\nYes, the Dictionary is Completely Full.";
-                else 
-                    cout<<"\nNo, the Dictionary is Not full.";
-                break; 
             case -1 : cout<<"\nAre you sure, you want to clear dictionary (y/n) ? ";
                 cin>>choice; 
                 if(choice=='y'||choice=='Y') 
@@ -303,5 +237,250 @@ int main()
  
 /* OUTPUT 
 
+Program to implement Dictionary using Hash Table.
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ 1 to Insert new Word
+ 2 to Display Dictionary
+ 3 to Search a word
+ 4 to Delete a word
+ -1 to Clear whole Dictionary
+  0 to Quit
+Enter your choice : 1
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Enter Entry Details :-
+Enter Word      : computer
+Enter Meaning   : pc
+
+Word inserted Successfully!
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ 1 to Insert new Word
+ 2 to Display Dictionary
+ 3 to Search a word
+ 4 to Delete a word
+ -1 to Clear whole Dictionary
+  0 to Quit
+Enter your choice : 1
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Enter Entry Details :-
+Enter Word      : laptop
+ laptop
+Enter Meaning   : notebook
+
+Word inserted Successfully!
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ 1 to Insert new Word
+ 2 to Display Dictionary
+ 3 to Search a word
+ 4 to Delete a word
+ -1 to Clear whole Dictionary
+  0 to Quit
+Enter your choice : 1
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Enter Entry Details :-
+Enter Word      : eclipse
+Enter Meaning   : hide
+
+Word inserted Successfully!
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ 1 to Insert new Word
+ 2 to Display Dictionary
+ 3 to Search a word
+ 4 to Delete a word
+ -1 to Clear whole Dictionary
+  0 to Quit
+Enter your choice : 2
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+                Dictionary
+---------------------------------------------
+Word - Meaning
+---------------------------------------------
+
+eclipse - hide,
+
+
+
+
+laptop - notebook,
+
+
+computer - pc,
+---------------------------------------------
+MAX is 10
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ 1 to Insert new Word
+ 2 to Display Dictionary
+ 3 to Search a word
+ 4 to Delete a word
+ -1 to Clear whole Dictionary
+  0 to Quit
+Enter your choice : 1
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Enter Entry Details :-
+Enter Word      : structure
+Enter Meaning   : organisation
+
+Word inserted Successfully!
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ 1 to Insert new Word
+ 2 to Display Dictionary
+ 3 to Search a word
+ 4 to Delete a word
+ -1 to Clear whole Dictionary
+  0 to Quit
+Enter your choice : 1
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Enter Entry Details :-
+Enter Word      : data
+Enter Meaning   : statistics
+
+Word inserted Successfully!
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ 1 to Insert new Word
+ 2 to Display Dictionary
+ 3 to Search a word
+ 4 to Delete a word
+ -1 to Clear whole Dictionary
+  0 to Quit
+Enter your choice : 2
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+                Dictionary
+---------------------------------------------
+Word - Meaning
+---------------------------------------------
+data - statistics,
+eclipse - hide,
+
+
+
+
+laptop - notebook,
+
+
+computer - pc, structure - organisation,
+---------------------------------------------
+MAX is 10
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ 1 to Insert new Word
+ 2 to Display Dictionary
+ 3 to Search a word
+ 4 to Delete a word
+ -1 to Clear whole Dictionary
+  0 to Quit
+Enter your choice : 3
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Enter word to Search : structure
+
+Word    : structure
+Meaning : organisation
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ 1 to Insert new Word
+ 2 to Display Dictionary
+ 3 to Search a word
+ 4 to Delete a word
+ -1 to Clear whole Dictionary
+  0 to Quit
+Enter your choice : 4
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Enter word to Delete : computer
+
+Word deleted Successfully!
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ 1 to Insert new Word
+ 2 to Display Dictionary
+ 3 to Search a word
+ 4 to Delete a word
+ -1 to Clear whole Dictionary
+  0 to Quit
+Enter your choice : 2
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+                Dictionary
+---------------------------------------------
+Word - Meaning
+---------------------------------------------
+data - statistics,
+eclipse - hide,
+
+
+
+
+laptop - notebook,
+
+
+structure - organisation,
+---------------------------------------------
+MAX is 10
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ 1 to Insert new Word
+ 2 to Display Dictionary
+ 3 to Search a word
+ 4 to Delete a word
+ -1 to Clear whole Dictionary
+  0 to Quit
+Enter your choice : 4
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Enter word to Delete : structure
+
+Word deleted Successfully!
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ 1 to Insert new Word
+ 2 to Display Dictionary
+ 3 to Search a word
+ 4 to Delete a word
+ -1 to Clear whole Dictionary
+  0 to Quit
+Enter your choice : 2
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+                Dictionary
+---------------------------------------------
+Word - Meaning
+---------------------------------------------
+data - statistics,
+eclipse - hide,
+
+
+
+
+laptop - notebook,
+
+
+
+---------------------------------------------
+MAX is 10
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ 1 to Insert new Word
+ 2 to Display Dictionary
+ 3 to Search a word
+ 4 to Delete a word
+ -1 to Clear whole Dictionary
+  0 to Quit
+Enter your choice : 0
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Thank you for using this program :)
  
 */
